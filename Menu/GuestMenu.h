0231c347da0ef78baaf80f520a40e556e0e77ddd
Nguyễn Nguyên Khương <s3924577@rmit.edu.vn>
2023-01-07 22:47:17 +0700

    fix bug and update util

--- Menu/GuestMenu.h
+++ Menu/GuestMenu.h
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <functional>
 #include "Menu.h"
+#include "../utils/ReadString.h"
 using namespace std;
 
 class GuestMenu : public Menu{
@@ -16,7 +17,8 @@ class GuestMenu : public Menu{
         :Menu({}, houseList, memberList, houseReviewList, memberReviewList, requestList){
             this->addOption(Option("1", "Register to become a member", [&]() -> void {signUp();}));
             this->addOption(Option("2", "View all house details (not including reviews and availability)", [&]() -> void {viewAllHouseDetails();}));
-            this->addOption(Option("3", "Exit the program", [&]() -> void {Menu::stop();}));
+            this->addOption(Option("3", "Return to main menu"));
+            this->addOption(Option("4", "Exit the program", [&]() -> void {Menu::stop();}));
         };
 
         void signUp(){
@@ -25,13 +27,13 @@ class GuestMenu : public Menu{
             string fullName;
             string phoneNumber;
             cout << "Enter username: ";
-            cin >> username;
+            readString(username);
             cout << "Enter password: ";
-            cin >> password;
+            readString(password);
             cout << "Enter full name: ";
-            cin >> fullName;
+            readString(fullName);
             cout << "Enter phone number: ";
-            cin >> phoneNumber;
+            readString(phoneNumber);
             int id = memberList.size();
             Member member(id, username, password, fullName, phoneNumber);
             memberList.push_back(member);
